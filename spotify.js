e1b8f55187773e1aa73325b04e6935ca53c8b0e5
Eli <55drk1@gmail.com>
2015-08-04 21:51:08 +0200

    Second iteration, adding albums to artists

--- spotify.js
+++ spotify.js
@@ -29,6 +29,7 @@
 	  								'<dt>'+ items[i].name + '</dt><br>',
 	  								'<img src="' + items[i].images[0].url+ '" height="200px">',
 	  							'</p>',
+	  							'<p><button type="button" num="'+ items[i].id +'" class="button-album" data-toggle="modal" data-target="#myModal">Albums</button></p>',
 	  						'</ul>',
 	  					'</div>'
 	  					].join('\n');
@@ -50,3 +51,53 @@
 		}
 
 		$('.submitart').on('click', getArtist);
+
+
+
+  				function showAlbum(event)
+				{		
+					event.preventDefault();
+					var idartist = $(this).attr('num');
+					var request = $.get('https://api.spotify.com/v1/artists/'+idartist+'/albums');
+
+					function albumsWorking(albums)
+					{
+						var items = albums.items;
+						var i = 0;
+						for (var i = 0; i < items.length-1; i++)
+  						{
+	  					 	if (items[i].images[0] != undefined) 
+	  				 	{
+	  				 	var htmlalbum = 
+	  					[
+	  					
+	  							'<div style="display: inline-block ;margin: 12px">',
+	  								'<dt>'+ items[i].name + '</dt><br>',
+	  								'<img src="' + items[i].images[0].url+ '" height="200px">',
+	  							'</div>',	
+	  		
+	  					].join('\n');
+
+	  					$('.modal-body').append(htmlalbum);
+	  					}
+	  				 };
+					}
+
+					function albumsNotWorking (err1, err2, err3) 
+		  			{
+		    			console.error('OH NO!!', err1, err2, err3);
+		  			}
+
+					request.done(albumsWorking);
+		  			request.fail(albumsNotWorking);
+				}
+
+		$(document).on('click','.button-album', showAlbum);
+
+
+
+
+
+
+
+
